da8584b5eaadcc2fabf6e689f7a4fa5d8e51094c
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-08-31 19:15:22 +0600

    ecommerce redux app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ProductsListView from "./features/products/ProductsListView";
+
+function App() {
+  return (
+    <div>
+      <h1>This Is The Main Page</h1>
+      <ProductsListView/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/features/products/ProductsListView.jsx
@@ -0,0 +1,9 @@
+const ProductsListView = () => {
+    return (
+        <div>
+            <h1>bfjhgdshfgv</h1>
+        </div>
+    );
+};
+
+export default ProductsListView;
